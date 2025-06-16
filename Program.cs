72c61bac526600f1e52780054fd9fa7b9972df0b
Genildon Barreto <genildonbarreto@yahoo.com.br>
2025-06-16 10:01:54 -0300

    Atualizados os testes para as classes Nokia e Iphone no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
-Console.WriteLine("Smartphone Nokia");
-Smartphone nokia = new Nokia(numero: "00012", modelo: "Asha 501", imei: "1546878765423", memoria: 4);
+Console.WriteLine("\nSmartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "05511876", modelo: "Asha 501", imei: "46878765423", memoria: 4);
 nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSmartphone Iphone:");
+Smartphone iphone = new Iphone("05578543", "Iphone X", "97838494562", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+iphone.InstalarAplicativo("Telegram");
