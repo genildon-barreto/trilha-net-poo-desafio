12e27925b5e9f33ab73c78af54f8c56f03ff495d
Genildon Barreto <genildonbarreto@yahoo.com.br>
2025-06-16 09:51:46 -0300

    Adiciona testes para a classe Nokia no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "00012", modelo: "Asha 501", imei: "1546878765423", memoria: 4);
+nokia.Ligar();
